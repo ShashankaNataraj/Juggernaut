2c060dd659dcb665e0d2999118e7eb049cf501f1
Shashanka N <shasn@cisco.com>
2019-05-11 22:14:16 +0530

    Used Monaco

--- src/web/app.js
+++ src/web/app.js
@@ -31,7 +31,38 @@ window.addEventListener('DOMContentLoaded',function() {
 		const editor = monaco.editor.create(document.getElementById("editor"), {
 			value: content,
 			language: "javascript"
-		});	
+		});
+		editor.addAction({
+			// An unique identifier of the contributed action.
+			id: 'my-unique-id',
+
+			// A label of the action that will be presented to the user.
+			label: 'My Label!!!',
+
+			// An optional array of keybindings for the action.
+			keybindings: [
+				monaco.KeyMod.CtrlCmd | monaco.KeyCode.F10,
+				// chord
+				monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_K, monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_M)
+			],
+
+			// A precondition for this action.
+			precondition: null,
+
+			// A rule to evaluate on top of the precondition in order to dispatch the keybindings.
+			keybindingContext: null,
+
+			contextMenuGroupId: 'navigation',
+
+			contextMenuOrder: 1.5,
+
+			// Method that will be executed when the action is triggered.
+			// @param editor The editor instance is passed in as a convinience
+			run: function(ed) {
+				alert("i'm running => " + ed.getPosition());
+				return null;
+			}
+		});
 		const statusNode = document.getElementById('status'),
 			vimMode = MonacoVim.initVimMode(editor, statusNode);
 	};
